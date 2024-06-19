6de1749e9f31a3ecde506bafe5bbb56caea6832b
Dwi Gustin Nurdialit <dwiknrd@gmail.com>
2024-06-19 15:17:16 +0700

    feat: update assets

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+st.write(df)
