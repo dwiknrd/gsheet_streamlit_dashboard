1e4b040bb581fe550d1813b1eda77c82c421bf59
Dwi Gustin Nurdialit <dwiknrd@gmail.com>
2024-06-19 16:54:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(
+    worksheet="tips", # nama worksheet yang ingin diambil
+    ttl="10m" # waktu cache
+)
 
 # Print results.
 st.write(df)
