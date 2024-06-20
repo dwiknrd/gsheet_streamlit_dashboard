6dfb2ed5119b72c7c9782f211d92a65ba091664e
Dwi Gustin Nurdialit <dwiknrd@gmail.com>
2024-06-20 16:36:14 +0700

    ydata_profiling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 import pandas as pd 
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import time
 
