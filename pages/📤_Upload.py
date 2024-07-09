9e2bb5478bd313fe1430b1af410f571a8adb1a0e
Dwi Gustin Nurdialit <dwiknrd@gmail.com>
2024-07-09 21:39:48 +0700

    Update 📤_Upload.py

--- pages/📤_Upload.py
+++ pages/📤_Upload.py
@@ -6,12 +6,14 @@ from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import time
 
+import os
+import sys
+import base64
+
 st.set_page_config(page_title='Data Profiler',layout='wide', page_icon='📝')
 
 
-
 st.markdown("<h1 style='text-align: center;'>📤 Upload Your Data</h1>", unsafe_allow_html=True)
-
 st.markdown("---")
 
 
@@ -29,23 +31,22 @@ def validate_file(file):
         return False
     
 
+# Function to generate profile report
+def generate_report(df):
+    with st.spinner('Generating Report'):
+        pr = ProfileReport(df)
+    return pr
+
+# Initialize session state for df and report if not already done
+if 'df' not in st.session_state:
+    st.session_state.df = None
+if 'report' not in st.session_state:
+    st.session_state.report = None
+    
+
 # sidebar
 with st.sidebar:
     uploaded_file = st.file_uploader("Upload .csv, .xlsx files not exceeding 10 MB")
-    if uploaded_file is not None:
-        st.write('Modes of Operation')
-        minimal = st.checkbox('Do you want minimal report ?')
-        display_mode = st.radio('Display mode:',
-                                options=('Primary','Dark','Orange'))
-        if display_mode == 'Dark':
-            dark_mode= True
-            orange_mode = False
-        elif display_mode == 'Orange':
-            dark_mode = False
-            orange_mode = True
-        else:
-            dark_mode = False
-            orange_mode = False
         
     
 if uploaded_file is not None:
@@ -61,17 +62,22 @@ if uploaded_file is not None:
                 sheet_tuple = tuple(xl_file.sheet_names)
                 sheet_name = st.sidebar.selectbox('Select the sheet',sheet_tuple)
                 df = xl_file.parse(sheet_name)
-                
-                
-            # generate report
-            with st.spinner('Generating Report'):
-                pr = ProfileReport(df,
-                                minimal=minimal,
-                                dark_mode=dark_mode,
-                                orange_mode=orange_mode
-                                )
-                
-            st_profile_report(pr)
+
+            # Save the dataframe in session state
+            st.session_state.df = df
+
+            # Generate and save the report in session state
+            st.session_state.report = generate_report(df)
+
+            # Display the report in the main page
+            st_profile_report(st.session_state.report)    
+
+            # Download button for HTML report
+            html_report_str = st.session_state.report.to_html()
+            b64 = base64.b64encode(html_report_str.encode()).decode()
+            href = f'<a href="data:text/html;base64,{b64}" download="report.html">Click here to download report</a>'
+            st.sidebar.markdown(href, unsafe_allow_html=True)
+
         else:
             st.error(f'Maximum allowed filesize is 10 MB. But received {filesize} MB')
             
@@ -81,3 +87,12 @@ if uploaded_file is not None:
 else:
     st.info('Upload your data in the left sidebar to generate profiling')
     
+# Display the report if it exists in the session state
+if st.session_state.report is not None:
+    st_profile_report(st.session_state.report, key="profile_report_main_existing")
+
+    # # Download button for HTML report in sidebar
+    # html_report_str = st.session_state.report.to_html()
+    # b64 = base64.b64encode(html_report_str.encode()).decode()
+    # href = f'<a href="data:text/html;base64,{b64}" download="report.html">Click here to download report</a>'
+    # st.sidebar.markdown(href, unsafe_allow_html=True)
